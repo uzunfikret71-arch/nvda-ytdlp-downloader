41eae77cd4b8b4e30415ec93d526adb259e3817f
uzunfikret71-arch <uzunfikret71@gmail.com>
2026-06-05 11:45:56 +0300

    Use GPLv2-compatible header and explicit subprocess shell handling

--- .addon/globalPlugins/ytdlpDownloader.py
+++ .addon/globalPlugins/ytdlpDownloader.py
@@ -1,4 +1,20 @@
 # -*- coding: UTF-8 -*-
+#
+# ytdlpDownloader: NVDA add-on for downloading video and audio with yt-dlp.
+# Copyright (C) 2026 Fikret Uzun
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import os
 import subprocess
@@ -15,6 +31,7 @@ BIN_DIR = os.path.join(ADDON_DIR, "bin")
 YTDLP_EXE = os.path.join(BIN_DIR, "yt-dlp.exe")
 FFMPEG_EXE = os.path.join(BIN_DIR, "ffmpeg.exe")
 DENO_EXE = os.path.join(BIN_DIR, "deno.exe")
+CREATE_NO_WINDOW = getattr(subprocess, "CREATE_NO_WINDOW", 0)
 
 
 class DownloadDialog(wx.Dialog):
@@ -147,10 +164,11 @@ class DownloadDialog(wx.Dialog):
 				[YTDLP_EXE, "-U"],
 				stdout=subprocess.PIPE,
 				stderr=subprocess.STDOUT,
+				shell=False,
 				text=True,
 				encoding="utf-8",
 				errors="replace",
-				creationflags=subprocess.CREATE_NO_WINDOW,
+				creationflags=CREATE_NO_WINDOW,
 				env=self.buildProcessEnv(),
 			)
 			for line in self.updateProcess.stdout:
@@ -241,10 +259,11 @@ class DownloadDialog(wx.Dialog):
 				command,
 				stdout=subprocess.PIPE,
 				stderr=subprocess.STDOUT,
+				shell=False,
 				text=True,
 				encoding="utf-8",
 				errors="replace",
-				creationflags=subprocess.CREATE_NO_WINDOW,
+				creationflags=CREATE_NO_WINDOW,
 				env=self.buildProcessEnv(),
 			)
 			for line in self.process.stdout:
